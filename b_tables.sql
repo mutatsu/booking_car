ecab203eff2bf39a8066b0e49228be8de5d041ce
masahiko.utatsu <utatu@u311.sakura.ne.jp>
2019-12-26 01:22:23 +0900

    first ddl

---
+++ b_tables.sql
@@ -0,0 +1,140 @@
+CREATE TABLE b_stocks (
+  stock_id                      NUMBER NOT NULL, -- sequence
+  car_id                        NUMBER NOT NULL, -- from b_cars
+  dealer_type                   VARCHAR2(2) NOT NULL,   -- 取扱系列: N,P,NP
+  manage_id                     NUMBER NOT NULL, -- 系列、車種毎の在庫番号
+  format_name                   VARCHAR2(200) NOT NULL, -- 型式
+  color_cd                      VARCHAR2(20)  NOT NULL, -- 色(マスター無し)
+  grade                         VARCHAR2(100) NOT NULL, -- グレード
+  option_list                   VARCHAR2(200) , -- 複数のオプションをカンマ区切りで入力
+  production_date               DATE NOT NULL,  -- 生産日
+  registration_expiry_date      DATE NOT NULL,  -- 完切日：完成検査修了証の有効期限が切れる日？
+  exhibit_flg                   VARCHAR2(1) DEFAULT 'N', -- 展示有無 展示:Y
+  df_cd                         VARCHAR2(20) NOT NULL,  -- DF 例: 05971, C2414 など
+  rank_cd                       VARCHAR2(20) NOT NULL,  -- ランク 例: A-1, A-2, D など
+  measures_comments             VARCHAR2(4000), -- 対策費
+  remarks                       VARCHAR2(4000), -- 備考
+  in_preparation_flg            VARCHAR2(1) DEFAULT 'N', -- Y: 準備中（一時的に予約可能物件として表示できなくする）
+  delete_flg                    VARCHAR2(1) DEFAULT 'N',
+  created                       DATE DEFAULT CURRENT_DATE,
+  created_by                    VARCHAR2(100) DEFAULT 'admin',
+  updated                       DATE DEFAULT CURRENT_DATE,
+  updated_by                    VARCHAR2(100) DEFAULT 'admin',
+  reservation_date              DATE,
+  reservation_deadline          DATE,
+  shop_id                       NUMBER,
+  user_id                       VARCHAR2(8),
+  close_flg                     VARCHAR2(1) DEFAULT 'N',
+  CONSTRAINT b_stocks_pk PRIMARY KEY (stock_id) ENABLE
+);
+create unique index b_stocks_idx01 on b_stocks(car_id,dealer_type,manage_id);
+create sequence b_stocks_seq start with 1 increment by 1 nocache order;
+
+CREATE TABLE b_cars (
+  car_id                        NUMBER NOT NULL,
+  car_name                      VARCHAR2(200) NOT NULL,
+  compact_flg                   VARCHAR2(1) NOT NULL,   -- N:not compact car, Y:compact car
+  display_num                   NUMBER,                 -- display order
+  delete_flg                    VARCHAR2(1) DEFAULT 'N',
+  created                       DATE DEFAULT CURRENT_DATE,
+  created_by                    VARCHAR2(100) DEFAULT 'admin',
+  updated                       DATE DEFAULT CURRENT_DATE,
+  updated_by                    VARCHAR2(100) DEFAULT 'admin',
+  CONSTRAINT b_cars_pk PRIMARY KEY (car_id) ENABLE
+);
+create sequence b_cars_seq start with 1 increment by 1 nocache order;
+
+CREATE TABLE b_users (
+  user_id                       VARCHAR2(8) NOT NULL, -- LOWER CASE
+  user_name                     VARCHAR2(100) NOT NULL,
+  shop_id                       NUMBER NOT NULL,
+  email_address                 VARCHAR2(100) NOT NULL, -- LOWER CASE
+  password                      VARCHAR2(255) NOT NULL,
+  password_life_time            NUMBER NOT NULL, -- 単位: 日
+  change_password               VARCHAR2(1) DEFAULT 'Y',
+  admin_flg                     VARCHAR2(1) DEFAULT 'N',
+  delete_flg                    VARCHAR2(1) DEFAULT 'N',
+  created                       DATE DEFAULT CURRENT_DATE,
+  created_by                    VARCHAR2(100) DEFAULT 'admin',
+  updated                       DATE DEFAULT CURRENT_DATE,
+  updated_by                    VARCHAR2(100) DEFAULT 'admin',
+  CONSTRAINT b_users_pk PRIMARY KEY (user_id) ENABLE
+);
+
+CREATE TABLE b_dealers (
+  dealer_id                     NUMBER NOT NULL,
+  dealer_cd                     VARCHAR2(2) NOT NULL,
+  dealer_name                   VARCHAR2(200) NOT NULL,
+  display_num                   NUMBER,
+  delete_flg                    VARCHAR2(1) DEFAULT 'N',
+  created                       DATE DEFAULT CURRENT_DATE,
+  created_by                    VARCHAR2(100) DEFAULT 'admin',
+  updated                       DATE DEFAULT CURRENT_DATE,
+  updated_by                    VARCHAR2(100) DEFAULT 'admin',
+  CONSTRAINT b_dealers_pk PRIMARY KEY (dealer_id) ENABLE
+);
+create sequence b_dealers_seq start with 1 increment by 1 nocache order;
+
+CREATE TABLE b_shops (
+  shop_id                       NUMBER NOT NULL,
+  shop_cd                       VARCHAR2(3) NOT NULL,
+  shop_name                     VARCHAR2(200) NOT NULL,
+  dealer_id                     NUMBER NOT NULL,
+  display_num                   NUMBER,
+  delete_flg                    VARCHAR2(1) DEFAULT 'N',
+  created                       DATE DEFAULT CURRENT_DATE,
+  created_by                    VARCHAR2(100) DEFAULT 'admin',
+  updated                       DATE DEFAULT CURRENT_DATE,
+  updated_by                    VARCHAR2(100) DEFAULT 'admin',
+  CONSTRAINT b_shops_pk PRIMARY KEY (shop_id) ENABLE
+);
+create sequence b_shops_seq start with 1 increment by 1 nocache order;
+
+CREATE TABLE b_stocks_log (
+  log_id               NUMBER NOT NULL,
+  stock_id                      NUMBER NOT NULL, -- sequence
+  car_id                        NUMBER NOT NULL, -- from n_cars
+  dealer_type                   VARCHAR2(2) NOT NULL,   -- 取扱系列: N,P,NP
+  manage_id                     NUMBER NOT NULL, -- 車種毎の管理番号
+  format_name                   VARCHAR2(200) NOT NULL, -- 型式
+  color_cd                      VARCHAR2(20)  NOT NULL, -- 色(マスター無し)
+  grade                         VARCHAR2(100) NOT NULL, -- グレード
+  option_list                   VARCHAR2(200) , -- 複数のオプションをカンマ区切りで入力
+  production_date               DATE NOT NULL,  -- 生産日
+  registration_expiry_date      DATE NOT NULL,  -- 完切日：完成検査修了証の有効期限が切れる日？
+  exhibit_flg                   VARCHAR2(1) DEFAULT 'N', -- 展示有無 展示:Y
+  df_cd                         VARCHAR2(20) NOT NULL,  -- DF 例: 05971, C2414 など
+  rank_cd                       VARCHAR2(20) NOT NULL,  -- ランク 例: A-1, A-2, D など
+  measures_comments             VARCHAR2(4000), -- 対策費
+  remarks                       VARCHAR2(4000), -- 備考
+  in_preparation_flg            VARCHAR2(1) DEFAULT 'N', -- Y: 準備中（一時的に予約可能物件として表示できなくする）
+  reservation_date              DATE,
+  reservation_deadline          DATE,
+  shop_id                       NUMBER,
+  user_id                       VARCHAR2(8),
+  close_flg                     VARCHAR2(1) DEFAULT 'N',
+  event_id                      VARCHAR2(1), -- Timeout:0、予約:1、予約キャンセル:2、成約:3、成約取消:4
+  event_date                    DATE,
+  CONSTRAINT b_stocks_log_pk PRIMARY KEY (log_id) ENABLE
+);
+create index b_stocks_log_idx01 on b_stocks_log(event_date,shop_id,user_id);
+create sequence b_stocks_log_seq start with 1 increment by 1 nocache order;
+
+CREATE TABLE b_codes ( 
+  id                            NUMBER        NOT NULL,
+  tag                           VARCHAR2(100) NOT NULL,
+  name                          VARCHAR2(100) NOT NULL,
+  vc                            VARCHAR2(100) ,
+  vn                            NUMBER,
+  memo                          VARCHAR2(100),
+  display_num                   NUMBER,
+  delete_flg                    VARCHAR2(1) DEFAULT 'N',
+  created                       DATE DEFAULT CURRENT_DATE,
+  created_by                    VARCHAR2(100) DEFAULT 'admin',
+  updated                       DATE DEFAULT CURRENT_DATE,
+  updated_by                    VARCHAR2(100) DEFAULT 'admin',
+  CONSTRAINT b_codes_pk PRIMARY KEY (id) ENABLE
+);
+create index b_codes_idx01 on b_codes(tag,name,vn,vc);
+create sequence b_codes_seq start with 1 increment by 1 nocache order;
+
---
+++ b_test_data.sql
@@ -0,0 +1,123 @@
+-- b_dealers
+insert into b_dealers (dealer_id, dealer_cd, dealer_name, display_num) values(b_dealers_seq.nextval,'01','京都トヨペット',1);
+insert into b_dealers (dealer_id, dealer_cd, dealer_name, display_num) values(b_dealers_seq.nextval,'02','ネッツトヨタ京華',2);
+
+-- b_shops
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'01','P本部');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,2,'02','N本部');
+
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','七条本店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','北店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','岡崎店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','カドノ店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','桂店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','山科店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','伏見店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','桃山店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','U-mix');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','Custom Garage');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','亀岡店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','舞鶴店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','峰山店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','福知山店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','マイカーガーデン');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','乙訓店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','城陽店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','久御山店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','木津川台店');
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name)values(b_shops_seq.nextval,1,'xx','レクサス西大路');
+
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name,display_num)values(b_shops_seq.nextval,2,33,'高野店', 1);
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name,display_num)values(b_shops_seq.nextval,2,34,'カドノ店', 2);
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name,display_num)values(b_shops_seq.nextval,2,31,'吉祥院店', 3);
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name,display_num)values(b_shops_seq.nextval,2,35,'山科店', 4);
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name,display_num)values(b_shops_seq.nextval,2,36,'伏見店', 5);
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name,display_num)values(b_shops_seq.nextval,2,51,'宇治店', 6);
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name,display_num)values(b_shops_seq.nextval,2,52,'久御山店', 7);
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name,display_num)values(b_shops_seq.nextval,2,53,'木津川台店', 8);
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name,display_num)values(b_shops_seq.nextval,2,54,'福知山店', 9);
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name,display_num)values(b_shops_seq.nextval,2,55,'舞鶴店', 10);
+insert into b_shops (shop_id,dealer_id,shop_cd,shop_name,display_num)values(b_shops_seq.nextval,2,56,'京丹後店', 11);
+
+-- b_users
+INSERT INTO b_users (shop_id,user_id,email_address,user_name,password,admin_flg,password_life_time)
+VALUES (1,'admin01','admin01@foo.com','管理者01',dbms_obfuscation_toolkit.md5(input_string => 'Welcome01'),'Y',0);
+INSERT INTO b_users (shop_id,user_id,email_address,user_name,password,admin_flg,password_life_time)
+VALUES (2,'admin02','admin02@foo.com','管理者02',dbms_obfuscation_toolkit.md5(input_string => 'Welcome01'),'Y',0);
+
+INSERT INTO b_users (shop_id,user_id,email_address,user_name,password,admin_flg,password_life_time)
+VALUES (3,'user03','user03@foo.com','利用者03',dbms_obfuscation_toolkit.md5(input_string => 'Welcome01'),'N',180);
+INSERT INTO b_users (shop_id,user_id,email_address,user_name,password,admin_flg,password_life_time)
+VALUES (4,'user04','user04@foo.com','利用者04',dbms_obfuscation_toolkit.md5(input_string => 'Welcome01'),'N',180);
+
+-- b_cars
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'アクア','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ヴィッツ','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'カローラ スポーツ','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'スペイド','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'タンク','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'パッソ','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ポルテ','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ヤリス','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ルーミー','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'アルファード','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ヴェルファイア','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ヴォクシー','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'エスクァイア','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'グランエース','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'シエンタ','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ノア','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ハイエース ワゴン','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'アリオン','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'カムリ','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'カローラ','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'カローラ アクシオ','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'クラウン','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'センチュリー','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'プリウス','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'プリウスPHV','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'プレミオ','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'MIRAI','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'カローラ ツーリング','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'カローラ フィールダー','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'プリウスα','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'C-HR','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ハイラックス','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ハリアー','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ライズ','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'RAV4','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ランドクルーザー','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ランドクルーザー プラド','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'コペン GR SPORT','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'スープラ','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'86','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'アクア GR SPORT','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ヴィッツ GR','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ヴィッツ GR SPORT','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ヴォクシー GR SPORT','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'コペン GR SPORT','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'C-HR GR SPORT','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'スープラ','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ノア GR SPORT','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'86 GR','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'86 GR SPORT','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ハリアー GR SPORT','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'プリウスα GR SPORT','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'プリウスPHV GR SPORT','N');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ピクシス エポック','Y');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ピクシス ジョイ','Y');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ピクシス トラック','Y');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ピクシス バン','Y');
+insert into b_cars (car_id,car_name,compact_flg)values(b_cars_seq.nextval,'ピクシス メガ','Y');
+
+-- b_codes
+insert into b_codes (id, tag, name, vc,display_num,memo)values(b_codes_seq.nextval,'DELETE_FLG',' ','N',1,'削除フラグ');
+insert into b_codes (id, tag, name, vc,display_num,memo)values(b_codes_seq.nextval,'DELETE_FLG','削除','Y',2,'削除フラグ');
+insert into b_codes (id, tag, name, vc,display_num,memo)values(b_codes_seq.nextval,'USERS$ADMIN_FLG',' ','N',1,'管理者フラグ');
+insert into b_codes (id, tag, name, vc,display_num,memo)values(b_codes_seq.nextval,'USERS$ADMIN_FLG','管理者','Y',2,'管理者フラグ');
+insert into b_codes (id, tag, name, vc,display_num,memo)values(b_codes_seq.nextval,'USERS$CHANGE_PASSWORD','変更なし','N',1,'次回パスワード変更フラグ');
+insert into b_codes (id, tag, name, vc,display_num,memo)values(b_codes_seq.nextval,'USERS$CHANGE_PASSWORD','次回変更','Y',2,'次回パスワード変更フラグ');
+insert into b_codes (id, tag, name, vc,display_num,memo)values(b_codes_seq.nextval,'CARS$COMPACT_FLG',' ','N',1,'軽自動車フラグ');
+insert into b_codes (id, tag, name, vc,display_num,memo)values(b_codes_seq.nextval,'CARS$COMPACT_FLG','軽','Y',2,'軽自動車フラグ');
+insert into b_codes (id, tag, name, vc,display_num,memo)values(b_codes_seq.nextval,'STOCKS$IN_PREPARATION_FLG',' ','N',1,'準備中フラグ');
+insert into b_codes (id, tag, name, vc,display_num,memo)values(b_codes_seq.nextval,'STOCKS$IN_PREPARATION_FLG','準備中','Y',2,'準備中フラグ');
